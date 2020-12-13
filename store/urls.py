d17a7ec5f59c7c4fb9bf282f6a1f1e946616daac
Rajeev vishwakarma <rvishwakarma00786@gmail.com>
2020-12-13 21:48:01 +0530

    upload

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.store, name="store"),
+    path('checkout/',views.checkout, name="checkout"),
+    path('cart/',views.cart, name="cart"),
+    path('update_item/',views.UpdateItem, name="update_item"),
+    path('process_order/',views.ProcessOrder, name="process_order"),
+    path('user_logout/',views.userLogout, name = "user_logout"),
+    path('login_register/', views.loginOrRegister,name="login_register"),
+
+]
